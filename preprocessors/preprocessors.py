186bdc6a835ff08b2f68ac9376f6e0b2c9f74b12
RybaPila-IT <r.radziukiewicz2@gmail.com>
2022-01-12 11:40:43 +0100

    Add preprocessors for data passed to models
    Preprocessors preprocess data for model in order
    to maintain readability and easy-to-use feature.
    Preprocessors support basic and advanced model.

---
+++ preprocessors/preprocessors.py
@@ -0,0 +1,76 @@
+import pandas as pd
+
+NEW_GROUPS = [
+    'Gry komputerowe',
+    'Gry na konsole',
+    'Sprzęt RTV',
+    'Komputery',
+    'Telefony i akcesoria'
+]
+
+SEPARATOR = ';'
+
+
+def _cast_category_path(category_path):
+    categories = category_path.split(SEPARATOR)
+    found_groups = [group for group in NEW_GROUPS if group in categories]
+    if len(found_groups) != 1:
+        raise RuntimeError('wrong group cast: {}'.format(found_groups))
+    return found_groups[0]
+
+
+def _preprocess_products_for_advanced_model(products_df: pd.DataFrame) -> pd.DataFrame:
+    product_drop_columns = [
+        "price",
+        "product_name",
+        "user_rating"
+    ]
+    new_products_df = products_df.drop(columns=product_drop_columns)
+    new_products_df["category_path"] = new_products_df["category_path"].apply(_cast_category_path)
+    return new_products_df
+
+
+def _preprocess_sessions_for_advanced_model(sessions_df: pd.DataFrame) -> pd.DataFrame:
+    session_drop_columns = [
+        "timestamp",
+        "event_type",
+        "offered_discount",
+        "purchase_id",
+        "session_id"
+    ]
+    return sessions_df.drop(columns=session_drop_columns)
+
+
+def preprocess_data_for_advanced_model(
+        sessions_df: pd.DataFrame,
+        products_df: pd.DataFrame
+        ) -> (pd.DataFrame, pd.DataFrame):
+    return (
+        _preprocess_sessions_for_advanced_model(sessions_df=sessions_df),
+        _preprocess_products_for_advanced_model(products_df=products_df)
+    )
+
+
+def preprocess_data_for_basic_model(
+        sessions_df: pd.DataFrame,
+        products_df: pd.DataFrame
+        ) -> (pd.DataFrame, pd.DataFrame):
+    product_drop_columns = [
+        "product_name",
+        "category_path",
+        "price"
+    ]
+    session_drop_columns = [
+        "timestamp",
+        "event_type",
+        "offered_discount",
+        "purchase_id",
+        "session_id",
+        "user_id"
+    ]
+    new_sessions = sessions_df.drop(columns=session_drop_columns)
+    new_products = products_df.drop(columns=product_drop_columns)
+    return (
+        new_sessions,
+        new_products
+    )
