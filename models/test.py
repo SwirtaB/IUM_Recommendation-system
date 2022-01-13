1f6953988468ea8554d8a01112534beac3b2128d
RybaPila-IT <r.radziukiewicz2@gmail.com>
2022-01-13 12:06:02 +0100

    Implement models testing functionality

--- models/test.py
+++ models/test.py
@@ -4,6 +4,30 @@ from preprocessors import preprocessors
 from basic import build as build_basic
 from advanced import build as build_advanced
 
+"""
+    Code present in this file is responsible for models testing, both basic and advanced.
+    
+    Metric used for testing purposes is accuracy.
+    
+    Test and train set creation algorithm:
+        - From all sessions pick those, which have some bigger events count (ex. 8).
+        - From picked sessions choose BATCH_SIZE adjacent activities.
+        - Add those 3 adjacent activities to test set.
+        - Finally, form train set as the set difference of sessions set - test set.
+        
+    Model evaluation algorithm:
+        - Sort test set by session_id column.
+        - Iterate over test batch.
+            - For each activity in batch, generate the model prediction.
+            - Check, if prediction contains the next product which user had interaction with within the batch.
+            - If yes, increment good predictions counter.
+        - Calculate model accuracy. 
+"""
+
+
+# Batch is the adjacent activities count within the session.
+BATCH_SIZE = 3
+
 
 # Picks sessions having the size equal to or greater than n_min value.
 def _sessions_with_min_size(sessions_df: pd.DataFrame, n_min: int = 8) -> pd.DataFrame:
@@ -19,13 +43,12 @@ def _form_test_set(big_sessions_df: pd.DataFrame) -> pd.DataFrame:
         data=None,
         columns=big_sessions_df.columns
     )
-    # Below loop picks 3 adjacent events from each big session,
+    # Below loop picks BATCH_SIZE adjacent events from each big session,
     # and appends them to the test set.
-    # Printing is used for verbose communication with the user.
     for session_key in events_grouped_by_session.groups.keys():
         single_session = events_grouped_by_session.get_group(session_key)
-        picked_idx = random.randint(0, single_session.shape[0] - 3)
-        test_set = test_set.append(single_session.iloc[picked_idx: picked_idx+3])
+        picked_idx = random.randint(0, single_session.shape[0] - BATCH_SIZE)
+        test_set = test_set.append(single_session.iloc[picked_idx: picked_idx+BATCH_SIZE])
 
     return test_set
 
@@ -36,14 +59,14 @@ def _form_train_set(sessions_df: pd.DataFrame, test_set: pd.DataFrame) -> pd.Dat
 
 
 def _train_test_split_sessions_data(sessions_df: pd.DataFrame) -> (pd.DataFrame, pd.DataFrame):
-    ################################################################
-    # Group sessions by session_id                                 #
-    # Select only sessions which has more than 8 activities in it. #
-    # From each selected pick at random 3 adjacent activities.     #
-    # Picked activities will form test set.                        #
-    # From the rest of session data, erase picked activities.      #
-    # Sessions with erased activities will form train set.         #
-    ################################################################
+    #####################################################################
+    # Group sessions by session_id                                      #
+    # Select only sessions which has more than 8 activities in it.      #
+    # From each selected pick at random BATCH_SIZE adjacent activities. #
+    # Picked activities will form test set.                             #
+    # From the rest of session data, erase picked activities.           #
+    # Sessions with erased activities will form train set.              #
+    #####################################################################
 
     # Big sessions stores sessions with size (aka. events_count) larger than 8.
     big_sessions = _sessions_with_min_size(sessions_df)
@@ -56,6 +79,29 @@ def _train_test_split_sessions_data(sessions_df: pd.DataFrame) -> (pd.DataFrame,
     return train_set, test_set
 
 
+def _test_model(test_set: pd.DataFrame, model):
+    ###################################################################
+    # For each session activity without the last, perform prediction. #
+    # Check, if next seen product is present in prediction obtained.  #
+    # If it is, increment correct predictions.                        #
+    # Metric used is Accuracy (good_predictions / all_predictions).   #
+    ###################################################################
+    test_set.sort_values(by="session_id", inplace=True)
+    good_predictions, all_predictions = 0, (test_set.shape[0] / BATCH_SIZE) * (BATCH_SIZE - 1)
+    for i in range(0, test_set.shape[0] - BATCH_SIZE, BATCH_SIZE):
+        for j in range(0, BATCH_SIZE):
+            current_session_action = test_set.iloc[i+j]
+            predictions = model.recommend(
+                user_id=current_session_action["user_id"],
+                category=current_session_action["category_path"]
+            )
+            if test_set.iloc[i+j+1]["product_id"] in predictions:
+                good_predictions += 1
+
+    accuracy = (good_predictions / all_predictions) * 100
+    print("{} recommender achieved {:.2f} accuracy.".format(model.name(), accuracy))
+
+
 if __name__ == "__main__":
     sessions_df_fp = "../data/sessions.jsonl"
     products_df_fp = "../data/products.jsonl"
@@ -64,12 +110,16 @@ if __name__ == "__main__":
     productsDF = pd.read_json(products_df_fp, lines=True)
 
     train_df, test_df = _train_test_split_sessions_data(sessions_df=sessionsDF)
-
+    # Adding category path column to the sessions in order to make predictions easier.
+    # Now there is no need to pass products DataFrame to _test_model method.
+    predictions_ready_test_set = preprocessors.preprocess_data_for_predictions(test_df, productsDF)
     print("Beginning to build models...")
-
     basic_train_set, basic_products_set = preprocessors.preprocess_data_for_basic_model(train_df, productsDF)
     basic_recommender = build_basic(basic_train_set, basic_products_set)
     print("Built basic recommender without errors...")
     adv_train_set, adv_products_set = preprocessors.preprocess_data_for_advanced_model(train_df, productsDF)
     advanced_recommender = build_advanced(adv_train_set, adv_products_set)
     print("Built advanced recommender without errors...")
+
+    _test_model(predictions_ready_test_set, basic_recommender)
+    _test_model(predictions_ready_test_set, advanced_recommender)
